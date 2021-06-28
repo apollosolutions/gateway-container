a776062b1b447ff544f30ea1e96c23a4e3cd2611
Lenny Burdette <lenny@apollographql.com>
2021-06-28 12:16:44 -0700

    feat: use wait-ready PM2 feature
    https://pm2.keymetrics.io/docs/usage/signals-clean-restart/

--- src/docker_entrypoint.sh
+++ src/docker_entrypoint.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env sh
 
 # use the precomposed supergraph from the uplink instead of the old runtime composition
+# TODO: remove when the uplink is the default
 export APOLLO_SCHEMA_CONFIG_DELIVERY_ENDPOINT=${APOLLO_SCHEMA_CONFIG_DELIVERY_ENDPOINT:-https://uplink.api.apollographql.com/}
 
-yarn pm2 start src/index.js -i ${PROCESS_COUNT:-max} --no-daemon --no-autorestart
+yarn pm2 start src/index.js -i ${PROCESS_COUNT:-max} --no-daemon --no-autorestart --wait-ready
