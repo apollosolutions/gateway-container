a143f10d0138ccbf6eb8da490de40e9b6453a298
Lenny Burdette <lenny@apollographql.com>
2021-06-29 13:52:05 -0700

    feat: cleanup deps and simplify health checks

--- src/index.js
+++ src/index.js
@@ -7,8 +7,7 @@ if (config.openTelemetry) {
   setupOpentelemetry(config.openTelemetry);
 }
 
-import express from "express";
-import { ApolloServer } from "apollo-server-express";
+import { ApolloServer } from "apollo-server";
 import { ApolloGateway } from "@apollo/gateway";
 import { bootstrap as bootstrapGlobalAgent } from "global-agent";
 
@@ -18,27 +17,14 @@ const gateway = new ApolloGateway(config.gateway);
 
 const server = new ApolloServer({
   ...config.server,
-  // @ts-ignore
   gateway,
   context(ctx) {
     return ctx;
   },
 });
 
-const app = express();
-
-app.get("/readyz", (_, res) => res.send("ok"));
-app.get("/livez", (_, res) => res.send("ok"));
-app.get("/healthz", (_, res) => res.send("ok"));
-
-await server.start();
-server.applyMiddleware({ app });
-
 const port = parseInt(config.server.port ?? process.env.PORT ?? "4000", 10);
 const host = config.server.host ?? process.env.HOST;
 
-await new Promise((resolve) =>
-  app.listen({ port, host }, () => resolve(undefined))
-);
-
-console.log(`Gateway running at ${host ?? "localhost"}:${port}`);
+const { url } = await server.listen({ port, host });
+console.log(`Gateway running at ${url}`);
